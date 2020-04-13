a81427e56fdfced23e2e264377ea9c0dcf57ef48
Roydon Misseldine <roydonmisseldine@gmail.com>
2020-04-13 11:28:30 +0100

    Site upload

---
+++ js/textcursor.js
@@ -0,0 +1,7 @@
+function custom(event){
+  var el = document.getElementById("hov");
+	el.style.top = event.clientY + "px";
+	el.style.left = event.clientX + "px";
+		}
+
+document.getElementById("main").addEventListener('mousemove', custom);
